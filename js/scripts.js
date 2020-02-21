93dcd7797978283a631c608e1c9044c0b5fad4a9
Wess58 <wesleymbate@gmail.com>
2020-02-21 10:31:21 +0300

    added test images for added libraries

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// $(window).scroll(function() {
+// //
+// if ($(window).scrollTop() >= 100) {
+//
+//   $(".rellax").eq($(this).index()).addClass("blur");
+// } else {
+//   $(".rellax").eq($(this).index()).removeClass("blur");
+// }
+// });
+
+$('.rellax').each(function() {
+  $(this).css({
+    "left": Math.random() * $(".container").outerHeight,
+    "left": Math.random() * $(".container-fluid").outerHeight,
+    "top": Math.random() * window.outerHeight
+
+  })
+});
